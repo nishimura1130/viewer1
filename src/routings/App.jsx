30274ae86cbea8b0878d9816704f632380ab7077
hogehoge <hogehoge@example.com>
2021-12-04 15:09:57 +0900

    Fix routings commentout review

--- src/routings/App.jsx
+++ src/routings/App.jsx
@@ -1,9 +1,14 @@
-//NAME: routingsについて
+// NAME: routingsについて
 // WHAT: ブラウザからサーバーにリクエストを受けて、サーバーからブラウザに表示させる処理
-//WHY: サーバーがリクエスト、レスポンスするようにできるために、そのリクエストを受け取るroutingのため。
-// HOW:   インスタンス化したapp, routerのgetメソッドを読んで引数にパス名、コールバックを描く。
-// HOW:   router直下に使用したいアプリケーションのコンポーネントを配置する。
+// WHY: サーバーがリクエスト、レスポンスするようにできるために、そのリクエストを受け取るroutingのため。
+// HOW: インスタンス化したapp, routerのgetメソッドを読んで引数にパス名、コールバックを描く。
+// HOW: router直下に使用したいアプリケーションのコンポーネントを配置する。
 
+// FIXED VERSION
+// NAME: routingsについて
+// WHAT: ブラウザからサーバーにリクエストを受けて、サーバーからレスポンスを返す処理。レスポンスの型がhtmlならブラウザに表示したり、APIならjsonを受け取るなど様々な使い方ができる。
+// WHY:  (HINT: index.jsでexpressを用いて、すでにroutingは書いてあるのになぜreact再度でも必要なのかを描くと良い )
+// HOW: routerコンポーネントに直下に使用したいのコンポーネントやURLなどのPropsを配置する。
 
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';

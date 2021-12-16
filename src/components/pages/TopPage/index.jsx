2f625732ec205da3b5cad323f4d414b79f14b8fa
hogehoge <hogehoge@example.com>
2021-12-16 16:11:39 +0900

    レンダリングとイベントハンドラーの仕組みがわからない

--- src/components/pages/TopPage/index.jsx
+++ src/components/pages/TopPage/index.jsx
@@ -1,5 +1,9 @@
 import React, { useEffect, useRef, useState } from 'react';
 import PropTypes from 'prop-types';
+
+// ここを追加する
+import axios from 'axios';
+
 import VideosListTemplate from '~/components/templates/VideosListTemplate';
 import Header from '~/components/organisms/Header';
 import SearchForm from '~/components/organisms/SearchForm';
@@ -13,11 +17,11 @@ export const TopPagePresenter = ({
   loading,
 }) => (
   <VideosListTemplate
-    headerContents={<Header/>}
+    headerContents={<Header />}
     searchFormContents={(
       <SearchForm onSubmit={search} defaultValue={defaultKeyword} />
     )}
-    videosListContents={<VideosList videos={videos} loading={loading} /> }
+    videosListContents={<VideosList videos={videos} loading={loading} />}
     onScrollEnd={searchNext}
   />
 );
@@ -36,7 +40,101 @@ TopPagePresenter.defaultProps = {
   defaultKeyword: '',
 };
 
-export default TopPagePresenter;
+// ここから追加修正
+const TopPageContainer = ({
+  api,
+  presenter,
+  defaultKeyword,
+}) => {
+  const [videos, setVideos] = useState([]);
+  const [nextPageToken, setNextPageToken] = useState(null);
+  const [keyword, setKeyword] = useState(defaultKeyword);
+  const [loading, setLoading] = useState(false);
+  const cleanedUp = useRef(false);
 
-// NAME: Presenter
-// WHAT: 
+  /**
+   * ビデオの取得
+   * @param pageToken 続きを取得する場合は前回取得時のレスポンスに含まれるnextPageTokenを指定する
+   * @returns {Promise<void>}
+   */
+  const getVidúeos = async (pageToken) => {
+    setLoading(true);　
+    const {
+      data: {
+        items,
+        nextPageToken: newNextPageToken,
+      },
+    } = await api.search(keyword, { pageToken });
+    if (cleanedUp.current) {
+      return;
+    }
+    let nextVideos;
+    if (pageToken) {
+      // 重複を取り除く
+      const itemsWithoutDuplicated = items.filter(
+        ({ id: itemId }) => !videos.find(({ id }) => id === itemId),
+      );
+      nextVideos = videos.concat(itemsWithoutDuplicated);
+    } else {
+      nextVideos = items;
+    }
+    setVideos(nextVideos);
+    setNextPageToken(newNextPageToken);
+    setLoading(false);
+  };
+
+
+  //NAME: レンダリング
+  // WHAT: 現在のpropsとstateを元に、Reactがコンポーネントに対して、それらがどのように見えるべきなのか尋ねるプロセス。
+  // WHY: 
+  // HOW:
+  // NAME: useEffect
+  // WHAT: 
+  // keywordが変更されたらビデオ取得する
+  useEffect(() => {
+    setNextPageToken(undefined);
+    setVideos([]);
+    getVideos();
+  }, [keyword]);
+
+  // コンポーネントがunmountされたらそれを覚えておく
+  useEffect(() => (() => {
+    cleanedUp.current = true;
+  }), []);
+
+  return presenter({
+    search: setKeyword,
+    searchNext: () => {
+      if (loading || !nextPageToken) {
+        // 現在ロード中、または次のページがない場合は何もしない
+        return;
+      }
+      getVideos(nextPageToken);
+    },
+    defaultKeyword,
+    videos,
+    loading,
+  });
+};
+
+TopPageContainer.propTypes = {
+  api: PropTypes.shape({
+    search: PropTypes.func,
+  }),
+  defaultKeyword: PropTypes.string,
+  presenter: PropTypes.func.isRequired,
+};
+
+TopPageContainer.defaultProps = {
+  api: {
+    search: (keyword, params) => axios.get(`/api/videos/search/${keyword}`, { params }),
+  },
+  defaultKeyword: '遠藤さくら',
+};
+
+export default (props) => (
+  <TopPageContainer
+    presenter={TopPagePresenter}
+    {...props}
+  />
+);
